1e0975ff8c2c52af4d0d8d2f86010c3298855fc2
rdsoard <srebekahd@gmail.com>
2021-05-01 17:33:38 -0400

    updating homepage

--- representativeApp/src/app/app-routing.module.ts
+++ representativeApp/src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
