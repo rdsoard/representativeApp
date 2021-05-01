bc6e5275c2c8bfb466911ea08a20e077b945d68f
rdsoard <srebekahd@gmail.com>
2021-05-01 17:59:39 -0400

    adding firebase service and updating homepage

--- representativeApp/src/app/home/home.page.ts
+++ representativeApp/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +9,24 @@ import { Geolocation } from '@ionic-native/geolocation';
 export class HomePage implements OnInit {
 
   constructor(private geolocation: Geolocation) { }
-
-  this.geolocation.getCurrentPosition().then((resp) => {
-	// resp.coords.latitude
-	// resp.coords.longitude
-   }).catch((error) => {
-	 console.log('Error getting location', error);
-   });
    
 
   ngOnInit() {
   }
 
+  allowLocation(){
+	this.geolocation.getCurrentPosition().then((resp) => {
+		// resp.coords.latitude
+		// resp.coords.longitude
+	   }).catch((error) => {
+		 console.log('Error getting location', error);
+	   });
+	   
+	   let watch = this.geolocation.watchPosition();
+	   watch.subscribe((data) => {
+		// data can be a set of coordinates, or an error (if an error occurred).
+		// data.coords.latitude
+		// data.coords.longitude
+	   });
+  }
 }
