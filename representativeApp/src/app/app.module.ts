a8dbab061c9e9110594a562b1705cbd04e48111f
rdsoard <srebekahd@gmail.com>
2021-05-02 18:20:33 -0400

    updating home

--- representativeApp/src/app/app.module.ts
+++ representativeApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -22,7 +23,7 @@ const firebaseConfig = {
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [NativeGeocoder, Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- representativeApp/src/app/home/home.page.ts
+++ representativeApp/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,29 +10,43 @@ import { Router } from '@angular/router';
 })
 export class HomePage implements OnInit {
 
-  constructor(private geolocation: Geolocation,private router: Router) { }
-   
+  constructor(private geolocation: Geolocation,private router: Router,private nativeGeocoder: NativeGeocoder) { 
+
+  }
+
+   zipcode:any;
+   lat:any;
+   longt:any;
 
   ngOnInit() {
 	 
   }
 
+  searchZip(){
+	  console.log(this.zipcode)
+	  this.router.navigate(["/list"],this.zipcode);
+  }
+
+//this gives the longitude and lattitude, need to convert that to a zipcode and set that = to this.zipcode
+
   allowLocation(){
 	this.geolocation.getCurrentPosition().then((resp) => {
-		// resp.coords.latitude
-		// resp.coords.longitude
+		this.lat = (resp.coords.latitude);
+ 		this.longt =(resp.coords.longitude);
 	   }).catch((error) => {
 		 console.log('Error getting location', error);
 	   });
-	   
-	   let watch = this.geolocation.watchPosition();
-	   watch.subscribe((data) => {
-		// data can be a set of coordinates, or an error (if an error occurred).
-		// data.coords.latitude
-		// data.coords.longitude
-		
-	   });
-	   this.router.navigate(["/list"]);
+	
+	let options: NativeGeocoderOptions = {
+		useLocale: true,
+		maxResults: 5
+	};
+
+	this.nativeGeocoder.reverseGeocode(this.lat, this.longt, options)
+	.then((result: NativeGeocoderResult[]) => console.log(JSON.stringify(result[0])))
+	.catch((error: any) => console.log(error));
+
+	this.router.navigate(["/list"],this.zipcode);
 
   }
 }
